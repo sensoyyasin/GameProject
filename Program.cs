9e419a50482a2e9bfb0de6a5e3b8af6a0aeae8e1
sensoyyasin <73845925+sensoyyasin@users.noreply.github.com>
2021-02-05 18:04:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+            gamerManager.Add(new Gamer
+            {
+                Id = 1,
+                BirthYear = 2001,
+                FirstName = "Yasin",
+                LastName = "Şensoy",
+                IdentityNumber = 12345
+            });
+        }
+    }
+}
---
+++ UserValidationManager.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+    class UserValidationManager : IUserValidationService
+    {
+        public bool Validate(Gamer gamer)
+        {
+            if (gamer.BirthYear == 2001 && gamer.FirstName == "Yasin"
+                && gamer.LastName == "Şensoy" && gamer.IdentityNumber == 12345)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
